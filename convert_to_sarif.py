bbea6e9ad63987947fe6a51f89804a2c845e7dd6
victorsilvazup <victor.silva@zup.com.br>
2024-07-19 00:56:38 -0300

    feat: add integration with github scanning report

---
+++ convert_to_sarif.py
@@ -0,0 +1,40 @@
+import os
+import json
+
+def convert_to_sarif(input_json, output_file):
+    sarif = {
+        "version": "2.1.0",
+        "runs": [
+            {
+                "tool": {
+                    "driver": {
+                        "name": "StackSpot AI",
+                        "informationUri": "https://ai.stackspot.com/docs/pt-br/quick-commands/create-remote-qc",
+                        "rules": [],
+                    }
+                },
+                "results": [],
+            }
+        ],
+    }
+
+    for result in input_json:
+        sarif_result = {
+            "ruleId": result["rule_id"],
+            "message": {"text": result["correction"]},
+            "locations": [
+                {
+                    "physicalLocation": {
+                        "artifactLocation": {"uri": result["file"]},
+                        "region": {"startLine": result["line"]},
+                    }
+                }
+            ],
+        }
+        sarif["runs"][0]["results"].append(sarif_result)
+
+    with open(output_file, "w") as f:
+        json.dump(sarif, f, indent=2)
+        
+INPUT_JSON = os.getenv("INPUT_JSON")
+convert_to_sarif(INPUT_JSON, "results.sarif")
